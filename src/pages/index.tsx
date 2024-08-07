f4d17ef3c583d83373a938098a32db78307dc123
GbCderr14 <114488917+GbCderr14@users.noreply.github.com>
2024-08-07 12:01:20 +0530

    completed process scheduling algos

---
+++ src/pages/index.tsx
@@ -0,0 +1,50 @@
+import Head from 'next/head';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Input from '../components/Input';
+import Output from '../components/Output';
+
+import { media } from '../components/GlobalStyle.css';
+
+const Main = styled.main`
+  display: flex;
+  ${media['1050']`flex-direction: column;`}
+  margin: 45px auto 1rem !important;
+  ${media['600']`margin: 20px auto 1rem !important`};
+  gap: clamp(0.5rem, 2.5vw, 4rem);
+  ${media['1050']`gap: 0.75rem`};
+`;
+
+export default function Home() {
+  const [selectedAlgo, setSelectedAlgo] = useState(null);
+  const [arrivalTime, setArrivalTime] = useState<number[]>([]);
+  const [burstTime, setBurstTime] = useState<number[]>([]);
+  const [timeQuantum, setTimeQuantum] = useState<number>();
+  const [priorities, setPriorities] = useState<number[]>([]);
+
+  return (
+    <div>
+      <Head>
+        <title>Process Scheduling Solver</title>
+      </Head>
+
+      <Main className="container">
+        <Input
+          selectedAlgo={selectedAlgo}
+          setSelectedAlgo={setSelectedAlgo}
+          setArrivalTime={setArrivalTime}
+          setBurstTime={setBurstTime}
+          setTimeQuantum={setTimeQuantum}
+          setPriorities={setPriorities}
+        />
+        <Output
+          selectedAlgo={selectedAlgo}
+          arrivalTime={arrivalTime}
+          burstTime={burstTime}
+          timeQuantum={timeQuantum}
+          priorities={priorities}
+        />
+      </Main>
+    </div>
+  );
+}
